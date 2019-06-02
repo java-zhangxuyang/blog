122dcf1248979100e461f6c3f823a4984a4147be
zxy <zxy163_work@163.com>
2019-06-02 17:22:38 +0800

    解决打包出错问题

--- src/main/java/com/zhangxy/front/controller/IndexControler.java
+++ src/main/java/com/zhangxy/front/controller/IndexControler.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.github.pagehelper.PageInfo;
 import com.zhangxy.base.utils.IPUtils;
@@ -22,6 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
+@RestController
 public class IndexControler {
 	
 	@Autowired
