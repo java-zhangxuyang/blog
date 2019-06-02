e4ee45d9d9b2c7840de72c3bd83766b50e658cad
zxy <zxy163_work@163.com>
2019-06-02 21:36:11 +0800

    添加beetl配置文件

--- src/main/java/com/zhangxy/BlogApplication.java
+++ src/main/java/com/zhangxy/BlogApplication.java
@@ -48,7 +48,7 @@ public class BlogApplication {
     @Bean(name = "beetlViewResolver")
     public BeetlSpringViewResolver getBeetlSpringViewResolver(@Qualifier("beetlConfig") BeetlGroupUtilConfiguration beetlGroupUtilConfiguration) {
         BeetlSpringViewResolver beetlSpringViewResolver = new BeetlSpringViewResolver();
-        beetlSpringViewResolver.setPrefix("templates/");
+        //beetlSpringViewResolver.setPrefix("templates/");
         beetlSpringViewResolver.setSuffix(".html");
         beetlSpringViewResolver.setContentType("text/html;charset=UTF-8");
         beetlSpringViewResolver.setOrder(0);
