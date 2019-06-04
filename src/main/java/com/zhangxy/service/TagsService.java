9aa76d93efe90df57feef9c8d88b6a2a6e2239e9
zhangxy <zxy163_work@163.com>
2019-06-04 11:01:31 +0800

    同上

---
+++ src/main/java/com/zhangxy/service/TagsService.java
@@ -0,0 +1,16 @@
+package com.zhangxy.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zhangxy.mapper.ContentMapper;
+
+@Service
+public class TagsService {
+
+	@Autowired
+	private ContentMapper conMapper;
+	
+	
+	
+}
