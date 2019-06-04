0b640748182020f7678b36846e5c82de29521e24
zhangxy <zxy163_work@163.com>
2019-06-04 10:53:52 +0800

    修改后端结构

---
+++ src/main/java/com/zhangxy/admin/controller/LoginController.java
@@ -0,0 +1,38 @@
+package com.zhangxy.admin.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zhangxy.admin.service.LoginSerivce;
+import com.zhangxy.base.controler.BaseController;
+import com.zhangxy.base.support.ResponseBo;
+import com.zhangxy.model.User;
+
+@RestController
+public class LoginController  extends BaseController{
+	
+	@Autowired
+	private LoginSerivce indexSerivce;
+	
+	@PostMapping("/login")
+	@ResponseBody
+	public Object login(User user, HttpServletRequest request) {
+		Integer error_count = cache.get("login_error_count");
+        if (error_count==null || error_count < 3) {
+            ResponseBo responseBo = indexSerivce.login(user,request);
+            if (responseBo.getCode() == -1) {
+                error_count = null == error_count ? 1 : error_count + 1;
+                cache.set("login_error_count", error_count, 10 * 60);
+            }else {
+            	cache.del("login_error_count");
+            }
+            return responseBo;
+        }
+        return ResponseBo.fail("您输入密码已经错误超过3次，请10分钟后尝试");
+	}
+
+}
---
+++ src/main/java/com/zhangxy/admin/service/AdminService.java
@@ -0,0 +1,8 @@
+package com.zhangxy.admin.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminService {
+
+}
