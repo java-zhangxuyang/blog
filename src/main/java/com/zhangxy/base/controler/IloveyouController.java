d5b7aca20fd3ac91b1896f5fa9de77b0716c0b48
zxy <zxy163_work@163.com>
2019-06-26 21:16:24 +0800

    某人， I Love You!

---
+++ src/main/java/com/zhangxy/base/controler/IloveyouController.java
@@ -0,0 +1,13 @@
+package com.zhangxy.base.controler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IloveyouController {
+
+	@GetMapping("/hxp")
+	public String render404() {
+        return "hxp/index";
+    }
+}
