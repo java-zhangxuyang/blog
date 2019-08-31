6adac8f780020f6bfb1b4d46dc311e29ab2ead28
zxy <zxy163_work@163.com>
2019-08-31 23:16:10 +0800

    修改服务器启动时间长 图片上传失败问题

--- src/main/java/com/zhangxy/BlogApplication.java
+++ src/main/java/com/zhangxy/BlogApplication.java
@@ -2,6 +2,8 @@ package com.zhangxy;
 
 import java.io.IOException;
 
+import javax.servlet.MultipartConfigElement;
+
 import org.beetl.core.resource.ClasspathResourceLoader;
 import org.beetl.core.resource.WebAppResourceLoader;
 import org.beetl.ext.spring.BeetlGroupUtilConfiguration;
@@ -13,6 +15,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.support.ResourcePatternResolver;
@@ -28,6 +31,20 @@ public class BlogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
+	
+	/**
+	     * 解决文件上传,临时文件夹被程序自动删除问题
+	     *
+	     * 文件上传时自定义临时路径
+	     * @return
+     */
+    @Bean
+    MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //2.该处就是指定的路径(需要提前创建好目录，否则上传时会抛出异常)
+        factory.setLocation("/data/uploadtmp");
+        return factory.createMultipartConfig();
+    }
 	
 	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/static/");
