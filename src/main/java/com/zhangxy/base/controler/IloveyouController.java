7ab92db745e2557e81657050ffbe428c01e64c87
zxy <zxy163_work@163.com>
2019-10-31 16:11:35 +0800

    添加 王雪莹 女神页

--- src/main/java/com/zhangxy/base/controler/IloveyouController.java
+++ src/main/java/com/zhangxy/base/controler/IloveyouController.java
@@ -6,13 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IloveyouController {
 
-	@GetMapping("/hxp")
-	public String hxp() {
-        return "hxp/index";
-    }
+	/*
+	 * @GetMapping("/hxp") public String hxp() { return "hxp/index"; }
+	 */
 	
 	@GetMapping("/ltq")
 	public String ltq() {
 		return "ltq/index";
+	}
+	
+	@GetMapping("/wxy")
+	public String wxy() {
+		return "wxy/index";
 	}
 }
